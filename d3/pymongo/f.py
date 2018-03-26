45f4a87bc17f71d9c7abd8eeb63685038986fe38
zhangdachuan <471841715@qq.com>
2018-03-26 13:08:31 +0800

    耶

--- d3/pymongo/f.py
+++ d3/pymongo/f.py
@@ -27,20 +27,38 @@ def change(args):
 def index():
 
     my_conn = MongoConn()
-    data1 = my_conn.db['nr2'].find({})
+    res = my_conn.db['nr6'].find({}).limit(5)
     data = []
     nodes = []
     links = []
-    for k in data1:
-        data.append(k)
-    for i in data:
-        nodes.extend(i["nodes"])
-        links.extend(i["relations"])
+
+    for i in range(5):
+        for k in res:
+            data.append(k)
+
+    #for i in data:
+        #nodes.extend(i["nodes"])
+        #links.extend(i["relations"])
+
+    nodes = data[1]["nodes"]
+    links = data[1]["relations"]
 
     data1 = json.dumps(nodes)
     data2 = json.dumps(links)
 
-    return render_template('graph.html',data1 = data1,data2 = data2 )
+
+    category = []
+    Category = []
+    for i in range(5):
+        temp = {}
+        category.append(data[i]["nodes"][0]["category"])
+        temp["name"] = data[i]["nodes"][0]["category"]
+        Category.append(temp)
+
+    data3 = json.dumps(category)
+    data4 = json.dumps(Category)
+
+    return render_template('graph.html',data1 = data1,data2 = data2,data3 = data3,data4 = data4)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1')
